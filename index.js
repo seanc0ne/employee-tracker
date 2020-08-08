12fb69f4ddbabf3ef776e58bcd79ccedc84c8b6c
Sean Cone <seandoesitbetter@gmail.com>
2020-08-08 14:41:27 -0700

    Updating index.js

--- index.js
+++ index.js
@@ -13,3 +13,46 @@ function init () {
     loadMainPrompts();
 }
 
+function loadMainPrompts() {
+    prompt ({
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?"
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "View All Employees By Department",
+                    value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+                },
+                {
+                    name: "View All Employees By Manager",
+                    value: "VIEW_EMPLOYEES_BY_MANAGER"
+                },
+                {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Remove Employee",
+                    value: "REMOVE_EMPLOYEE"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                },
+                {
+                    name: "Update Employee Manager",
+                    value: "UPDATE_EMPLOYEE_MANAGER"
+                },
+                {
+                    name: "View All Roles"
+                    value: "VIEW_ROLES"
+                }
+            ]
+        }
+    })
+}
