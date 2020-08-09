3526eb975de9a7b9ff0a1043075d02a042cd4127
Sean Cone <seandoesitbetter@gmail.com>
2020-08-09 12:21:20 -0700

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,22 +3,22 @@ use employees;
 INSERT INTO department
     (name)
 VALUES
-    ('Sales'),
-    ('Engineering'),
-    ('Finance'),
-    ('Legal');
+    ('Vocals'),
+    ('Guitars'),
+    ('Drums'),
+    ('Management');
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('Lead Singer', 200000, 1),
+    ('Backing Vocals', 90000, 1),
+    ('Lead Guitar', 175000, 2),
+    ('Rhythm Guitar', 150000, 2),
+    ('Drummer', 165000, 3),
+    ('Cowbell', 120000, 3),
+    ('Manager', 275000, 4),
+    ('Record Company Bozo', 200000, 4);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
