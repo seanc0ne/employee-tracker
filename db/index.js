87ea2d82faa5f10557d323639a33146f46927fc0
Sean Cone <seandoesitbetter@gmail.com>
2020-08-09 15:36:53 -0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,15 +1,22 @@
 const connection = require("./connection");
 
 const findAllEmployees = () => {
-    console.log('Finding all the employees...\n');
+    console.log('Finding all employees...\n');
     return connection.promise().query('SELECT * FROM employee');
 }
 
-const viewAllDep = () => {
-    console.log('Viewing all departments...\n');
-    
+const findAllDepartments = () => {
+    console.log('Finding all departments...\n');
+    return connection.promise().query('SELECT * FROM department');
+}
+
+const findAllRoles = () => {
+    console.log('Finding all roles...\n');
+    return connection.promise().query('SELECT * FROM role');
 }
 
 module.exports = {
-    findAllEmployees
+    findAllEmployees,
+    findAllDepartments,
+    findAllRoles
 };
